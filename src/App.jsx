60f1357e1dc49578bb37728edba003cd03f6a68e
David Raet <draet@vassar.edu>
2025-06-28 15:23:45 -0400

    feats:  - created a myJSON API to access character data  - uploaded images for each Blue Lock Character

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [character, setCharacter] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [banList, setBanList] = useState([]);
+
 
   return (
-    
+    <button>Discover</button>
   )
 }
 
